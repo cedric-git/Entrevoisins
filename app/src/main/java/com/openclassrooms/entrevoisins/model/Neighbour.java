71555797122f89589ba97d72ac82f10b6ff07cb1
anatole <irissouille@gmail.com>
2019-08-05 22:55:13 +0200

    - Temporary Neighbour Details page from UX Designer mock-up - Implement Neighbour model with new args (Adress, Phone, Email, About me --> to feed Details AND IsFavorite to prepare favorite list

--- app/src/main/java/com/openclassrooms/entrevoisins/model/Neighbour.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/model/Neighbour.java
@@ -16,16 +16,41 @@ public class Neighbour {
     /** Avatar */
     private String avatarUrl;
 
+    /** Adress */
+    private String address;
+
+    /** Phone */
+    private String phone;
+
+    /** WebSite */
+    private String webSite;
+
+    /** About Me */
+    private String aboutMe;
+
+    /** Favorite or not */
+    private boolean isFavorite;
+
     /**
      * Constructor
      * @param id
      * @param name
      * @param avatarUrl
+     * @param address
+     * @param phone
+     * @param webSite
+     * @param aboutMe
+     * @param isFavorite
      */
-    public Neighbour(Integer id, String name, String avatarUrl) {
+    public Neighbour(Integer id, String name, String avatarUrl, String address, String phone, String webSite, String aboutMe, boolean isFavorite) {
         this.id = id;
         this.name = name;
         this.avatarUrl = avatarUrl;
+        this.address = address;
+        this.phone = phone;
+        this.webSite = webSite;
+        this.aboutMe = aboutMe;
+        this.isFavorite = isFavorite;
     }
 
     public Integer getId() {
@@ -52,6 +77,21 @@ public class Neighbour {
         this.avatarUrl = avatarUrl;
     }
 
+    public String getAddress() { return address; }
+
+    public String getPhone() { return phone;}
+
+    public String getMail() { return webSite; }
+
+    public String getDescription() { return aboutMe; }
+
+    public boolean isFavorite() { return isFavorite; }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    //-----------------------------a completer ----------------------------------------//
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
